773bd8fe0527b3793441423b72f8870accc6006b
Sergei Vassiljev <servas@ttu.ee>
2020-06-16 21:10:01 +0300

    Added some code

--- server.js
+++ server.js
@@ -1,15 +1,22 @@
 const express = require('express');
 
 const app = express();
+const server = require('http').Server(app);
+const io = require('socket.io')(server);
 
 const rooms = new Map();
 
 app.get('/rooms', (req, res) => { //if get request then do this func
-    rooms.set('hello');
     res.json(rooms);
+    console.log("!")
 });
 
-app.listen(9999, (err) => {
+io.on('connection', socket => {
+    console.log('user connected', socket.id)
+})
+
+
+server.listen(9999, (err) => {
     if(err){
         throw Error(err);
     }

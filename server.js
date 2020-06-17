3db28294bcdbfde3352df0ed4abc4011f5f9a8db
Sergei Vassiljev <servas@ttu.ee>
2020-06-18 00:40:11 +0300

    adding some code

--- server.js
+++ server.js
@@ -4,22 +4,37 @@ const app = express();
 const server = require('http').Server(app);
 const io = require('socket.io')(server);
 
+app.use(express.json());
+
+
 const rooms = new Map();
 
 app.get('/rooms', (req, res) => { //if get request then do this func
     res.json(rooms);
-    console.log("!")
+});
+
+app.post('/rooms', (req, res) => {
+    const { roomId, userName } = req.body;
+    if (!rooms.has(roomId)) {
+        rooms.set(
+            roomId,
+            new Map([
+                ['users', new Map()],
+                ['messages', []],
+            ]),
+        );
+    }
+    res.send();
 });
 
 io.on('connection', socket => {
     console.log('user connected', socket.id)
-})
-
+});
 
 server.listen(9999, (err) => {
-    if(err){
+    if (err) {
         throw Error(err);
     }
-    console.log('Server is runnig');
+    console.log('Server is running');
 });
 
--- src/App.js
+++ src/App.js
@@ -1,13 +1,25 @@
 import React from 'react';
+
 import JoinBlock from './components/JoinBlock';
+import reducer from './reducer';
+
 
 function App() {
 
-  const connectSocket = () => {
-  }
+  const [state, dispatch] = React.useReducer(reducer, {
+    isAuth: false,
+  });
+
+  const onLogin = () => {
+    dispatch({
+      type: 'IS_AUTH',
+      payload: true,
+    });
+  };
+
   return (
     <div className="wrapper">
-      <JoinBlock/>
+      {!state.isAuth && <JoinBlock onLogin={onLogin} />}
     </div>
   );
 }
